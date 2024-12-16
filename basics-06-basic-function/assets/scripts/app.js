e1310aea1c428772d663a92dfb4d195e3356dedc
Andrei-Coloji <andrei.dandoci@flux.aero>
2024-12-16 10:46:15 +0200

    Lesson 30
    All the work up until 30

---
+++ basics-06-basic-function/assets/scripts/app.js
@@ -0,0 +1,16 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1,num2){
+    const result = num1 + num2
+    return result
+
+}
+
+add(1, 2)
+add(2, 4)
+currentResult = add(41,2)
+
+let calculationDescription = `${defaultResult} +10) * 3 / 2 -1`
+
+outputResult(currentResult, calculationDescription);
