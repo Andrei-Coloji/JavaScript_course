53e85368ac8af63482a77d13e2d2d68a6eb0b708
Andrei-Coloji <andrei.dandoci@flux.aero>
2024-12-17 15:19:23 +0200

    Ch3 Done

--- basics-06-basic-function/assets/scripts/app.js
+++ basics-06-basic-function/assets/scripts/app.js
@@ -1,12 +1,73 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function add(num1,num2){
-
-    currentResult = currentResult + userInput.value
-    outputResult(currentResult, '');
+function getUserInput() {
+  return parseInt(userInput.value);
 }
 
 
-addBtn.addEventListener('click', add)
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calDescription);
+}
+
+
+function add() {
+  const userInputedNumber = getUserInput();
+  const initResult = currentResult;
+  currentResult = currentResult + userInputedNumber;
+  createAndWriteOutput("+", initResult, userInputedNumber);
+
+  writeToLog("ADD", initResult, userInputedNumber, currentResult);
+}
+
+
+function subtract(num1, num2) {
+  const userInputedNumber = getUserInput();
+  const initResult = currentResult;
+  currentResult = currentResult - userInputedNumber;
+  createAndWriteOutput("-", initResult, userInputedNumber);
+
+  writeToLog("SUBTRACT", initResult, userInputedNumber, currentResult);
+}
+
+
+function multiply(num1, num2) {
+  const userInputedNumber = getUserInput();
+  const initResult = currentResult;
+  currentResult = currentResult * userInputedNumber;
+  createAndWriteOutput("*", initResult, userInputedNumber);
+  writeToLog("MULTIPLY", initResult, userInputedNumber, currentResult);
+}
+
+function divide(num1, num2) {
+  const userInputedNumber = getUserInput();
+  const initResult = currentResult;
+  currentResult = currentResult / userInputedNumber;
+  createAndWriteOutput("/", initResult, userInputedNumber);
+
+  writeToLog("DIVIDE", initResult, userInputedNumber, currentResult);
+}
+
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
+  ) {
+    const logEntry = {
+      operation: operationIdentifier,
+      previousResult: prevResult,
+      number: operationNumber,
+      result: newResult,
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+  }
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
